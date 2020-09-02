1ec11f033b2ef155d6fab050e1bc483bad0fdc9c
magicnana <magicnana999@126.com>
2020-09-02 14:46:34 +0800

    判空

--- src/main/java/com/creolophus/liuyi/common/json/JSON.java
+++ src/main/java/com/creolophus/liuyi/common/json/JSON.java
@@ -11,20 +11,33 @@ public class JSON {
     }
 
     public static <T> T parseObject(Object object,Class<T> clazz){
+
+        if(object == null || clazz == null){
+            return null;
+        }
         return JacksonUtil.toJava(object, clazz);
 
     }
 
     public static <T> T parseObject(byte[] bytes,Class<T> clazz){
+        if(bytes == null || bytes.length==0 || clazz == null){
+            return null;
+        }
         return JacksonUtil.toJava(bytes, clazz);
 
     }
 
     public static String toJSONString(Object object) {
+        if(object == null){
+            return null;
+        }
         return JacksonUtil.toJson(object);
     }
 
     public static byte[] toJSONBytes(Object object){
+        if(object == null){
+            return null;
+        }
         return JacksonUtil.toByteArray(object);
     }
 }
