7e8838cddc72ab3e80a1dd08f92f3cd0737a2a42
magicnana <magicnana999@126.com>
2021-02-04 15:00:17 +0800

    bug

--- src/main/java/com/creolophus/liuyi/common/security/GoAccessDeniedHandler.java
+++ src/main/java/com/creolophus/liuyi/common/security/GoAccessDeniedHandler.java
@@ -2,17 +2,16 @@ package com.creolophus.liuyi.common.security;
 
 import com.creolophus.liuyi.common.api.ApiResult;
 import com.creolophus.liuyi.common.json.JSON;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author magicnana
@@ -22,9 +21,18 @@ public class GoAccessDeniedHandler implements AccessDeniedHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GoAccessDeniedHandler.class);
     @Override
-    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse response, AccessDeniedException e) throws IOException {
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+        AccessDeniedException e) throws IOException {
 
-        logger.error("权限不够" + httpServletRequest.getRequestURI());
+        logger.error("权限不够" + request.getRequestURI());
+
+        // 此处配置的是允许任意域名跨域请求，可根据需求指定
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods",
+            "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setCharacterEncoding("UTF-8");
--- src/main/java/com/creolophus/liuyi/common/security/GoAuthenticationEntryPoint.java
+++ src/main/java/com/creolophus/liuyi/common/security/GoAuthenticationEntryPoint.java
@@ -2,17 +2,16 @@ package com.creolophus.liuyi.common.security;
 
 import com.creolophus.liuyi.common.api.ApiResult;
 import com.creolophus.liuyi.common.json.JSON;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author magicnana
@@ -23,10 +22,18 @@ public class GoAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private static final Logger logger = LoggerFactory.getLogger(GoAuthenticationEntryPoint.class);
 
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse response, AuthenticationException e) throws IOException {
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException e) throws IOException {
 
-        logger.error("尚未授权" + httpServletRequest.getRequestURI());
+        logger.error("尚未授权" + request.getRequestURI());
 
+        // 此处配置的是允许任意域名跨域请求，可根据需求指定
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods",
+            "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setCharacterEncoding("UTF-8");
